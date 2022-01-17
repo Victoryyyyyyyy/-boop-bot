facf9080afde3ff1305751a774b9b5b062442d87
Victoryyyyyyyy <victorarreguin86@gmail.com>
2022-01-16 17:37:48 -0800

    second

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const client = new Discord.Client({
 	intents:[
 		"GUILDS",
-		"GUILD_MESSAGES"
+		"GUILD_MESSAGES",
+		"GUILD_MEMBERS"
 	]
 });
 client.on("ready", () => {
@@ -14,4 +15,8 @@ client.on("messageCreate", (message) => {
 		message.reply("Hello world")
 	}
 });
+const welcomeChannelId ="932194485318877318"
+client.on("guildMemberAdd", (member) => {
+	member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+})
 client.login(process.env.TOKEN);
